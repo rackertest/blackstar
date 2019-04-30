e878af0e6c4cbf6c10f4129e983dbe94f7e2462d
rackertest <clone_ozone@yahoo.com>
2019-04-30 01:58:33 -0500

    Update blackstar.c

--- blackstar.c
+++ blackstar.c
@@ -1,23 +1,4 @@
-/*  This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
- 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
- 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
- 
-    I hereby claims all copyright interes in the program “blackstar.c”
-    Written by Jon Cox at cloneozone@gmail.com.
- 
-    You may compile this code with the following:
-    
-    gcc -O0 blackstar.c -o blackstar -lpthread -s */
-
+/* gcc -O0 blackstar.c -o blackstar -lpthread -s // Find why other optimization doesn't work. */
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -26,25 +7,15 @@
 #include <unistd.h>
 #include <pthread.h>
 
-/* counter for threads */
-int count = 0;
+//#define GetCurrentDir getcwd
 
 /* perform_work function args struct */
 typedef struct {
-    char *arg_1;
+    char arg_1[200];
 } arg_struct;
 
-union options {
-  char *user;
-  char *host;
-  char *pass;  
-  char *sshpass;
-  char *port; 
-  char *dcrypt;
-  char *ccrypt; 
-  char *input;
-  char *exec;
-};
+/* counter for threads */
+int count = 0;
 
 int usage(){
 	printf("\nUsage: ./blackstar [flags] [arguments]\n"
@@ -63,15 +34,16 @@ int usage(){
 /* function to execute in every thread */
 void *perform_work(void *argument){
    arg_struct *actual_args = argument;
-   system(actual_args->arg_1);
+   printf("%s\n", actual_args->arg_1);
+   //system(actual_args->arg_1);
    ++count;
 
    return NULL;
 }
 
 int file_open(char *name, char *comm){
-    int lines_allocated = 100;
-    int	max_line_len = 100;
+    int lines_allocated = 128;
+    int max_line_len = 200;
 
     /* allocate lines of text */
     char **words = (char **)malloc(sizeof(char*)*lines_allocated);
@@ -81,6 +53,12 @@ int file_open(char *name, char *comm){
     }
 
     FILE *fp = fopen(name, "r");
+    if (fp == NULL) {
+        fprintf(stderr,"Error opening default file.\n");
+	usage();
+        exit(2);
+    }
+
     int i;
     for (i=0;1;i++) {
         int j;
@@ -116,13 +94,13 @@ int file_open(char *name, char *comm){
     fclose(fp);
 
     pthread_t threads[i];
+    //int thread_args[i];
     int result_code, index;
     int *ptr[i];
 
     /* create all threads one by one */
     arg_struct *args = malloc(sizeof(arg_struct)*i);
-    for (index = 0; index < i; ++index) { 
-        args[index].arg_1 = malloc((sizeof(words) + sizeof(comm))*i);    
+    for (index = 0; index < i; ++index) {
         strcpy(args[index].arg_1, words[index]);
 	strcat(args[index].arg_1, comm);
         result_code = pthread_create(&threads[index], NULL, perform_work, &args[index]);
@@ -145,67 +123,54 @@ int file_open(char *name, char *comm){
 }
 
 int main (int argc, char **argv) {
-  unsigned short int fflag, lflag, uflag, wflag, pflag, eflag, oflag, dflag = 0;
-  short int opt = 0;
-
-  union options opt1, *ptr1 = &opt1;
-  union options opt2, *ptr2 = &opt2;
-  union options opt3, *ptr3 = &opt3;
-  union options opt4, *ptr4 = &opt4;
-  union options opt5, *ptr5 = &opt5;
-  union options opt6, *ptr6 = &opt6;
-  union options opt7, *ptr7 = &opt7;
-  union options opt8, *ptr8 = &opt8;
-  union options opt9, *ptr9 = &opt9;
-
-  opt1.input = NULL;
-  opt2.host = NULL;
-  opt3.user = NULL;
-  opt4.port = NULL;
-  opt5.pass = NULL;
-  opt6.dcrypt = NULL;
-  opt7.exec = NULL;
-  opt8.sshpass = NULL;
-  opt9.ccrypt = NULL;
+  int opt, fflag, lflag, uflag, wflag, pflag, eflag, oflag, dflag, sp = 0;
+  char *fname  = "./test";
+  char *passst = "sshpass -p '";
+  char *passcl = "' ";
+  char *empty;
+  char *skip = "-o StrictHostKeyChecking=no ";
+  char *exec, *user, *host, *pass, *sshpass, *port, *input, *dcrypt, *ccrypt;
+  
+  int base = 180;
 
 while(optind < argc) {
   if(( opt = getopt(argc, argv, "f:l:u:p:w:e:d:ho")) != -1){
    switch(opt){
      case 'f':
        fflag = 1;
-       ptr1->input=(char *)malloc(strlen(optarg));
-       strcpy(ptr1->input, optarg);
+       input=(char *)malloc(base);
+       strcpy(input, optarg);
        break;
      case 'l':
        lflag = 1;
-       ptr2->host=(char *)malloc(strlen(optarg));
-       strcpy(ptr2->host, optarg);
+       host=(char *)malloc(base);
+       strcpy(host, optarg);
        break;
      case 'u':
        uflag = 1;
-       ptr3->user=(char *)malloc(strlen(optarg));
-       strcpy(ptr3->user, optarg);
+       user=(char *)malloc(base);
+       strcpy(user, optarg);
        break;
      case 'p':
        pflag = 1;
-       ptr4->port=(char *)malloc(strlen(optarg));
-       strcpy(ptr4->port, optarg);
+       port=(char *)malloc(base);
+       strcpy(port, optarg);
        break;
      case 'w':
        wflag = 1;
-       ptr5->pass=(char *)malloc(strlen(optarg));
-       strcpy(ptr5->pass, optarg);
+       pass=(char *)malloc(base);
+       strcpy(pass, optarg);
        break;
      case 'd':
        dflag = 1;
-       ptr6->dcrypt=(char *)malloc(strlen(optarg));
-       strcpy(ptr6->dcrypt, optarg);
+       dcrypt=(char *)malloc(base);
+       strcpy(dcrypt, optarg);
        break;
      case 'e':
        eflag = 1;
-       ptr7->exec=(char *)malloc(strlen(optarg));
-       strcpy(ptr7->exec, " ");
-       strcat(ptr7->exec, optarg);
+       exec=(char *)malloc(base);
+       strcpy(exec, " ");
+       strcat(exec, optarg);
        break;
      case 'o':
        oflag = 1;
@@ -219,65 +184,65 @@ while(optind < argc) {
      }
    }
    else {
+      file_open(fname, exec);
       optind++;
    }
 }
 
    if(dflag == 1){
-	ptr9->ccrypt=(char *)malloc(strlen(ptr6->dcrypt) + 1);   
-	strcpy(ptr9->ccrypt, "ccrypt -d ");
-	strcat(ptr9->ccrypt, ptr6->dcrypt);
-	system(ptr9->ccrypt);
+	ccrypt=(char *)malloc(base);   
+	strcpy(ccrypt, "ccrypt -d ");
+	strcat(ccrypt, dcrypt);
+	system(ccrypt);
    }
 
-   if (fflag == 1 && eflag == 1){ 
-	file_open(ptr1->input, ptr7->exec);
+   if(fflag == 1 && eflag == 1){
+	file_open(input, exec);
    }
    else if(fflag == 0 && lflag == 1 && uflag == 1 && eflag == 1){
-	ptr8->sshpass=(char *)malloc((sizeof(ptr2->host) + sizeof(ptr3->user) + sizeof(ptr7->exec) + sizeof(ptr4->port) + sizeof(ptr5->pass))*4);
+	sp = 1;
+	sshpass=(char *)malloc(base);
 	if(wflag == 1){
-		strcpy(ptr8->sshpass, "sshpass -p '");
-		strcat(ptr8->sshpass, ptr5->pass);
-		strcat(ptr8->sshpass, "' ");
+		strcpy(sshpass, passst);
+		strcat(sshpass, pass);
+		strcat(sshpass, passcl);
 	}
-	strcat(ptr8->sshpass, "ssh ");
+	strcat(sshpass, "ssh ");
 	if(oflag == 1){
-		strcat(ptr8->sshpass, "-o StrictHostKeyChecking=no ");
+		strcat(sshpass, skip);
 	}
-	strcat(ptr8->sshpass, "-T ");
-	strcat(ptr8->sshpass, ptr3->user);
-	strcat(ptr8->sshpass, "@");
-	strcat(ptr8->sshpass, ptr2->host);
-	strcat(ptr8->sshpass, " '");
-	strcat(ptr8->sshpass, ptr7->exec);
-	strcat(ptr8->sshpass, "'");
+	strcat(sshpass, "-T ");
+	strcat(sshpass, user);
+	strcat(sshpass, "@");
+	strcat(sshpass, host);
+	strcat(sshpass, "'");
+	strcat(sshpass, exec);
+	strcat(sshpass, "'");
 	if(pflag == 1){
-		strcat(ptr8->sshpass, " -p ");
-		strcat(ptr8->sshpass, ptr4->port);
+		strcat(sshpass, " -p ");
+		strcat(sshpass, port);
 	}
-	system(ptr8->sshpass);
+	system(sshpass);
    }
    else{
 	usage();
    }	   
 
-   if(dflag == 1){ 
+   if(dflag == 1){
 	printf("*File was decrypted for usage. Now re-encrypt it.*\n");
-	ptr6->dcrypt[strlen(ptr6->dcrypt)-4] = 0;
-	strcpy(ptr9->ccrypt, "ccrypt ");
-        strcat(ptr9->ccrypt, ptr6->dcrypt);	
-	system(ptr8->sshpass);	
+	dcrypt[strlen(dcrypt)-4] = 0;
+	strcpy(ccrypt, "ccrypt ");
+        strcat(ccrypt, dcrypt);	
+	system(ccrypt);	
    }
 
-   if(!ptr1->input){free(ptr1->input);}
-   if(!ptr8->sshpass){free(ptr8->sshpass);} 
-   if(!ptr2->host){free(ptr2->host);} 
-   if(!ptr3->user){free(ptr3->user);} 
-   if(!ptr7->exec){free(ptr7->exec);} 
-   if(!ptr5->pass){free(ptr5->pass);} 
-   if(!ptr4->port){free(ptr4->port);} 
-   if(!ptr9->ccrypt){free(ptr9->ccrypt);} 
-   if(!ptr6->dcrypt){free(ptr6->dcrypt);}  
+   if(sp == 1){free(sshpass);}
+   if(lflag == 1){free(host);}
+   if(uflag == 1){free(user);}
+   if(eflag == 1){free(exec);}
+   if(wflag == 1){free(pass);}
+   if(pflag == 1){free(port);}
+   if(dflag == 1){free(ccrypt); free(dcrypt);}
 
   return 0;
 }
